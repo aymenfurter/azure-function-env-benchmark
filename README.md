e4fe178996ffcfb13fa004670f7fb138ac5265ae
Aymen Furter <aymen.furter@outlook.com>
2022-10-18 15:00:54 +0200

    feat: env variable benchmark
    * Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 A benchmark that measures the performance of reading environment variables in an Azure Function 
 
 This benchmark measures the performance of reading environment variables in an Azure Function. The test is performed by reading a variable from the Azure Function environment, using the System.getProperty method, and then measuring the time it takes to read the variable.
+
+
+## How to run
+```
+export VARIABLE_EXISTS=true && mvn package
+```
---
+++ src/test/java/envtest/FunctionTest.java
@@ -0,0 +1,53 @@
+package envtest;
+
+import com.microsoft.azure.functions.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ClearSystemProperty;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+import org.junitpioneer.jupiter.SetSystemProperty;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+
+public class FunctionTest {
+    @Test
+    public void testHttpTriggerJava() throws Exception {
+        String variableExisting = "VARIABLE_EXISTS";
+        String variableNotExisting = "VARIABLE_NOT_EXISTING";
+
+        assertNull(System.getenv(variableNotExisting));
+        assertNotNull(System.getenv(variableExisting));
+        
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(createHttpRequestMessageMock(), context);        
+
+        // Verify
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+    }
+
+    private HttpRequestMessage<Optional<String>> createHttpRequestMessageMock () {
+         @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        return req;
+         }
+}
